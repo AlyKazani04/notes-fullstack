10b928b4754645eab24bb75d4729dd1bbe9a3f93
Aly <aly.kazani52@gmail.com>
2026-07-27 20:47:12 +0500

    Removed TryCatch from User Queries

--- backend/src/db/userQueries.ts
+++ backend/src/db/userQueries.ts
@@ -1,31 +1,21 @@
 import { prisma as db } from './db.ts';
 
 export const getUserByID = async (id: number) => {
-  try {
-    const foundUser = await db.user
-      .findUnique({
-        where: { id },
-      });
+  const foundUser = await db.user
+    .findUnique({
+      where: { id },
+    });
 
-    return foundUser;
-  } catch (e) {
-    console.error('Failed to Find User\n', e);
-    throw e;
-  }
+  return foundUser;
 }
 
 export const getUserByEmail = async (email: string) => {
-  try {
-    const foundUser = await db.user
-      .findUnique({
-        where: { email },
-      });
+  const foundUser = await db.user
+    .findUnique({
+      where: { email },
+    });
 
-    return foundUser;
-  } catch (e) {
-    console.error('Failed to Find User\n', e);
-    throw e;
-  }
+  return foundUser;
 }
 
 export type UserSelect = {
@@ -35,20 +25,14 @@ export type UserSelect = {
 }
 
 export const insertUser = async (user: UserSelect) => {
-  try {
-    const res = await db.user
-      .create({
-        data: {
-          name: user.name,
-          email: user.email,
-          passwordHash: user.hashedPassword
-        }
-      });
+  const res = await db.user
+    .create({
+      data: {
+        name: user.name,
+        email: user.email,
+        passwordHash: user.hashedPassword
+      }
+    });
 
-    return res;
-  }
-  catch (e) {
-    console.error('Failed to Create User\n', e);
-    throw e;
-  }
+  return res;
 }
