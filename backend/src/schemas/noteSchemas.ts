7bd2126ab6a13391b746d6401c737a8ad14870f0
Aly <aly.kazani52@gmail.com>
2026-07-28 15:18:58 +0500

    Add Note Schemas

--- backend/src/schemas/noteSchemas.ts
+++ backend/src/schemas/noteSchemas.ts
@@ -1,8 +1,24 @@
 import { z } from 'zod';
 
-const createNoteSchema = z.object({
+export const folderQuerySchema = z.object({
+  folderId: z.number().int('Folder ID must be a positive integer').positive().nullable().optional()
+});
+
+export const createNoteSchema = z.object({
   title: z.string().min(1, 'Title cannot be empty').trim(),
   content: z.string().min(1, 'Content cannot be empty').trim(),
 });
 
-// TODO: Add other note schemas
+export const noteIdParamSchema = z.object({
+  id: z.string().regex(/^\d+$/, 'ID must be a valid number')
+});
+
+export const updateNoteSchema = z.object({
+  title: z.string().min(1, "Title cannct be empty").trim().optional(),
+  content: z.string().optional(),
+  folderId: z.number().int('Folder ID must be a positive integer').positive().nullable().optional()
+});
+
+export const batchDeleteNotesSchema = z.object({
+  noteIds: z.array(z.number().int('Note ID must be a positive integer').positive()).min(1, 'At least one Note ID must be provided')
+});
